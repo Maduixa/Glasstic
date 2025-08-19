7d6db84d973dc7bbc2e9ae53bdf618f5d9ec4163
Maduixa <joan.bcn.1990@gmail.com>
2025-08-19 22:35:11 +0100

    Initial Commit

---
+++ Glasstic/GlassticApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassticApp.swift
+//  Glasstic
+//
+//  Created by Hyper on 19/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlassticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
